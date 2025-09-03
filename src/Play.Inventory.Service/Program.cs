c606aa906bdcf0169e5e7805fc2a885446480863
Dharmeet Singh <dharmeetkhalsa@gmail.com>
2025-09-04 00:20:27 +0530

    PlayInventory service.

---
+++ src/Play.Inventory.Service/Program.cs
@@ -0,0 +1,26 @@
+using Play.Common.MongoDB;
+using Play.Inventory.Service.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
