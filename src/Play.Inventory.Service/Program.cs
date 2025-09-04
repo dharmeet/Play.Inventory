441326f93d1bc3d66fa6b83ca94dd37a6d0badde
Dharmeet Singh <dharmeetkhalsa@gmail.com>
2025-09-04 16:31:37 +0530

    Added retry policy.

--- src/Play.Inventory.Service/Program.cs
+++ src/Play.Inventory.Service/Program.cs
@@ -1,9 +1,32 @@
 using Play.Common.MongoDB;
+using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
+
+Random jitterer = new Random();
+
+builder.Services.AddHttpClient<CatalogClient>(httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://localhost:7261");
+})
+.AddTransientHttpErrorPolicy(b => b.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    5,
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+    onRetry: (outcome, timespan, retryAttempt) =>
+    {
+        // not recommended instead get the servie provider from the context.
+        var serviceProvider = builder.Services.BuildServiceProvider();
+        serviceProvider.GetService<ILogger<CatalogClient>>()?
+            .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}.");
+    }))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+
 builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
